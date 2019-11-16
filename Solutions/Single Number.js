7a30d3d9df22a6681ac56a4e76d4469d22ae2de0
jhsu42 <jhsu42@uwo.ca>
2019-11-15 22:14:58 -0800

    Single Number
    Solution to the Single Number problem on Leetcode.

---
+++ Solutions/Single Number.js
@@ -0,0 +1,10 @@
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+//Using the XOR operator, all elements that appear twice will be changed to 0, leaving the single number behind
+const singleNumber = (nums) => {
+    return nums.reduce((x,y) => x ^ y, 0)
+};
